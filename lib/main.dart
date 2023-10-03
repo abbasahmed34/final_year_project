fd3acfbd71a370b8fb2b471cbb4499f40352608d
abbasahmed34 <abbasahmed600@gmail.com>
2023-10-03 15:22:33 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:final_year_project/screens/chat.dart';
+import 'package:final_year_project/screens/splash.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+import 'package:final_year_project/screens/auth.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'FlutterChat',
+        theme: ThemeData().copyWith(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: const Color.fromARGB(255, 241, 141, 52)),
+        ),
+        home: StreamBuilder(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (ctx, snapshot){
+            if(snapshot.connectionState == ConnectionState.waiting){
+              return const SplashScreen();
+            }
+
+            if (snapshot.hasData){
+              return const ChatScreen();
+            }
+
+            return const AuthScreen();
+          },
+        ),
+    );
+  }
+}
